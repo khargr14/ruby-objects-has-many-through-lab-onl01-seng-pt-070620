7f8f92a158b1d1d08b140c02777416b5041ea2a7
Keesha Hargrove <kharg14@gmail.com>
2020-08-04 00:43:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,14 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  ALL = []
+  @@all = []
 
-  def initialize(name="", artist="", genre="")
-    @name = name
-    @artist = artist
-    @genre = genre
-    ALL << self
+  def initialize(name, artist, genre)
+    @name, @artist, @genre = name, artist, genre
+    @@all << self
   end
 
   def self.all
-    ALL
+    @@all
   end
-
-  def artist
-    @artist
-  end
-
-end 
+end
