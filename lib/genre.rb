002f28a2d40ac2aeb4f4f177f3c9f60fd011e6ce
Keesha Hargrove <kharg14@gmail.com>
2020-08-04 00:28:37 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,30 +1,22 @@
 class Genre
-
   attr_accessor :name
-
   @@all = []
-
 
   def initialize(name)
     @name = name
     @@all << self
   end
 
+  def songs
+    Song.all.select{|song| song.genre == self}
+  end
 
   def self.all
     @@all
   end
 
-  def songs
-    Song.all.select{|song| song.genre == self}
-  end
-
-  def new_song(name, artist)
-    Song.new(name, artist, self)
-  end
-
   def artists
-    songs.map{|song| song.artist}
+    self.songs.collect{|song| song.artist}.uniq
   end
 
-end
+end 
