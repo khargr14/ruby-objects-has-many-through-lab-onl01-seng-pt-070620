9df14b22b0b69441080268946d0cfbe9eca78edd
Keesha Hargrove <kharg14@gmail.com>
2020-08-04 00:46:18 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,30 +1,26 @@
 class Doctor
-  attr_accessor :name
+  attr_reader :name
 
-  @@all = []
+  @@all = Array.new
 
   def initialize(name)
-    @name = name    
+    @name = name
     @@all << self
-  end
-
-  def appointments
-    Appointment.all.select do |appointment|
-      appointment.doctor == self
-    end
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(date, patient, self)
-  end
-
-  def patients
-    appointments.collect do |appointment|
-      appointment.patient
-    end
   end
 
   def self.all
     @@all
   end
-end 
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.find_all {|apt| apt.doctor == self}
+  end
+
+  def patients
+    appointments.map {|ap| ap.patient}
+  end
+end
