fd41b4b5e78c24f743008e798802609b71851c41
Keesha Hargrove <kharg14@gmail.com>
2020-08-04 00:38:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,32 @@
 class Artist 
+  attr_accessor :name, :genre, :artist
 
-  attr_accessor :name
   @@all = []
 
   def initialize(name)
-    @name = name
-    @@all << self
+    @name = name 
+    @@all << self 
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-
-  def genres
-    songs.map {|song| song.genre}
-  end
-
-  def self.all
+  def self.all 
     @@all
   end
 
-  def new_song(song, genre)
-    Song.new(name, self, genre)
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
   end
 
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
 
 end 
